651e4ceacb8986b8161bc2914caad4f55841504b
Vincent <vincentgoossens@outlook.com>
2020-07-19 17:58:30 +0200

    fold notes

--- calcprobs.ts
+++ calcprobs.ts
@@ -5,13 +5,12 @@ var octProbs: number[] = [];
 var noteProbs: number[]  = [];
 var noteToggles: number[]  = [1,1,1,1,1,1,1,1,1,1,1,1];
 
-var probs: number[] = [];
-
 function list()
 {
 	switch (inlet) {
 		case 0:
 			octProbs = arrayfromargs(arguments);
+			octProbs.reverse();
 			break;
 		case 1:
 			noteProbs = arrayfromargs(arguments);
@@ -25,22 +24,26 @@ function list()
 
 function bang()
 {
-	const probs: number[] = [];
+	const used: number[] = [];
+	let note = 0;
 	const accumProbs: number[] = [];
 	let accum = 0;
 	for (let i = 0; i < octProbs.length; i++){
 		for (let j = 0; j < noteProbs.length; j++){
 			const prob=octProbs[i]*noteProbs[j]*noteToggles[j];
-			probs.push(prob);
+			if(prob>0){
+				used.push(note);
+			}
 			accum += prob;
 			accumProbs.push(accum);
+			note++;
 		}
 	}
 	for (let i = 0; i < accumProbs.length; i++){
 		accumProbs[i]=accumProbs[i]/accum;
 	}
 	
-	outlet(0, probs);
+	outlet(0, used);
 	outlet(1, accumProbs);
 }
 
