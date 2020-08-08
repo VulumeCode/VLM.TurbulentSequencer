96b08395403e84c7cd8c930fb6931c85ddd1eab2
Vincent <vincentgoossens@outlook.com>
2020-08-08 22:36:31 +0200

    external "pattr" for rotate

--- rotateSlider.ts
+++ rotateSlider.ts
@@ -1,27 +1,49 @@
-inlets = 1;
-outlets = 1;
+inlets = 2;
+outlets = 2;
 
 let values: number[] = [];
-let rotatedValues: number[] = [];
+
+let view: number[] = [];
+let viewLength = 12;
 let steps = 0;
 
 function msg_int(v: number)
 {
-    const diff = steps - v;
-    steps = v;
-
-    rotatedValues = values.slice(diff, values.length).concat(values.slice(0, diff));
+    const l = viewLength;
+    steps = (v + l) % l;
     bang();
 }
 
+function arrays_equal(a: any[],b: any[]) { return !!a && !!b && !(a<b || b<a); }
+
 function list()
 {
-    values = arrayfromargs(arguments);
+    switch (inlet) {
+		case 0:
+            const input = arrayfromargs(arguments);
+
+            values = input.slice(-steps, viewLength).concat(input.slice(0, -steps));
+
+            outlet(1, values);
+            outlet(0, "setlist " + view.join(' '));
+            break;
+        case 1:
+            const updatedValues = arrayfromargs(arguments);
+            if(arrays_equal(values, updatedValues)){
+                return;
+            }
+            else {
+                values = updatedValues;
+                bang();
+            }
+            break;
+    }
 }
 
 function bang()
 {
-    outlet(0, rotatedValues);
+    view = values.slice(steps, values.length).concat(values.slice(0, steps));
+    outlet(0, view);
 }
 
 
